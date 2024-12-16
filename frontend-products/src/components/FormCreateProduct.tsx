3b17ef5c6734b2b45b050c41e45284470ef6b444
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-12-16 03:35:04 -0600

    Complete form design

--- frontend-products/src/components/FormCreateProduct.tsx
+++ frontend-products/src/components/FormCreateProduct.tsx
@@ -1,9 +1,28 @@
 
 function FormCreateProduct() {
   return (
-    <div className="md:w-1/2 lg:w-2/5 mx-5 bg-slate-200">
-      Form
-    </div>
+    <form className="md:w-1/3 lg:w-2/5 mx-5 rounded-lg p-6 bg-gray-100">
+        <div className="">
+          <label htmlFor="base-input" className="block mb-2 font-medium">Name</label>
+          <input type="text" id="base-input" className="p-3 w-full block rounded-md shadow-md focus:ring-blue-500 focus:border-blue-500" />
+        </div>
+
+        <div className="mt-5">
+          <label htmlFor="base-input" className="block mb-2 font-medium">Price</label>
+          <input type="text" id="base-input" className="p-3 w-full block rounded-md shadow-md focus:ring-blue-500 focus:border-blue-500" />
+        </div>
+
+        <div className="mt-5">
+          <label htmlFor="message" className="block mb-2 font-medium">Description</label>
+          <textarea id="message" rows={4} className="p-3 w-full block rounded-md shadow-md focus:ring-blue-500 focus:border-blue-500 " placeholder="Leave a comment..."></textarea>
+        </div>
+
+        <input
+          type="submit"
+          className="mt-10 bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors rounded-lg"
+          value='Create'
+        />
+    </form>
   )
 }
 
--- frontend-products/src/components/ProductsTable.tsx
+++ frontend-products/src/components/ProductsTable.tsx
@@ -1,7 +1,7 @@
 
 function ProductsTable() {
   return (
-    <div className="md:w-1/2 lg:w-2/5 mx-5 bg-slate-200">
+    <div className="md:w-1/3 lg:w-3/5 mx-5 rounded-lg p-6 bg-gray-100">
       Table
     </div>
 
